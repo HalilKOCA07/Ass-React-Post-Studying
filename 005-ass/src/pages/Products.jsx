488b10146c19c4a789c4e305d6c32ec543451d28
Sinyour <sinyour52@gmail.com>
2024-04-19 01:55:07 +0300

    product card desing

--- 005-ass/src/pages/Products.jsx
+++ 005-ass/src/pages/Products.jsx
@@ -2,33 +2,53 @@ import { Ripple, initTWE } from "tw-elements";
 initTWE({ Ripple });
 
 const Products = () => {
-  return <div
-  class="block rounded-lg bg-white text-center text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white">
-  <div
-    class="border-b-2 border-neutral-100 px-6 py-3 dark:border-white/10">
-    Featured
-  </div>
-  <div class="p-6">
-    <h5 class="mb-2 text-xl font-medium leading-tight ">
-      Special title treatment
-    </h5>
-    <p class="mb-4 text-base ">
-      With supporting text below as a natural lead-in to additional
-      content.
-    </p>
-    <button
-      type="button"
-      class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
-      data-twe-ripple-init
-      data-twe-ripple-color="light">
-      Button
-    </button>
-  </div>
-  <div
-    class="border-t-2 border-neutral-100 px-6 py-3 text-surface/75 dark:border-white/10 dark:text-neutral-300">
-    2 days ago
-  </div>
-</div>
+
+  
+  return (
+    
+    <div className="block rounded-lg bg-blue-200 shadow-lg w-1/3 m-auto mt-6 text-center text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white">
+      <div class="text-3xl font-bold  border-b-2 border-neutral-100 px-6 py-3 dark:border-white/10">
+        product name
+      </div>
+      <div className="flex p-3 justify-evenly">
+        <div class="p-6">
+          <span className="text-red-700 font-bold text-2xl">$ 800.00</span>{" "}
+          <span className="line-through">$ 1000.00</span>
+          <div class="mb-4 text-base flex justify-center m-6 gap-4">
+            <button className=" bg-red-500 px-3 rounded-lg text-white text-2xl font-extrabold">
+              -
+            </button>
+            <p className="text-2xl font-extrabold">[ 5 ]</p>
+            <button className=" bg-green-500 px-3 rounded-lg text-white text-2xl font-extrabold">
+              +
+            </button>
+          </div>
+          <div className="mt-10">
+                      <span className=" font-bold">Total Price:</span><span>$ 4000.00</span>
+          </div>
+
+        </div>
+        <div className="flex justify-center ">
+          <img
+            className="w-[200px] h-[200px]"
+            src="https://st-troy.mncdn.com/mnresize/800/800/Content/media/ProductImg/original/mtpc3tua-apple-iphone-15-512gb-black-638305548989559998.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div class="border-t-2 border-neutral-100 px-6 py-3 text-surface/75 dark:border-white/10 dark:text-neutral-300 text-3xl font-bold">
+        <button
+          type="button"
+          className="bg-red-500 px-4 py-1 rounded-lg text-white hover:bg-red-600"
+          data-twe-ripple-init
+          data-twe-ripple-color="light"
+        >
+          Remove
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
